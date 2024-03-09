ae0222cbe1303eb81b2339f64a674dbf6fcfeb62
GrigAnd <andrey.grigorev03@gmail.com>
2024-03-09 11:50:06 +0300

    golden fixes

--- asm_src/prob5.asm
+++ asm_src/prob5.asm
@@ -1,35 +1,30 @@
         SECTION .data
-div:    db 10
+div:    db 0
 step:   db 2520
 x:      db 2520
-tmp:    db 2520
-c21:    db 21
+max:    db 21
 
         SECTION .text
-_loop20:
-        LD  x
-        ST  tmp
-        ST  step
-        LD div ; div++
+
+_inc_div:
+        LD div
         INC
         ST div
-
-        SUB c21 ; if div=21 goto _end
+        SUB max
         JZ _end
 
-_inc:
-        LD  x   ; x++
-        ADD step
-        ST  x
-        ST  tmp
-
-_sub:
-        LD tmp   ; x=x//div
+_loop:
+        LD x
         DIVR div
-        ST tmp
-        JZ  _loop20 ; if x=0 goto _loop20
-        JMP _inc ; else goto _inc
+        JZ _inc_div
+        LD x
+        ADD step
+        ST x
+        CLR
+        INC
+        ST div
+        JMP _loop
 
 _end:
         LD x
-        ST 4242
+        HLT
